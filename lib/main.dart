e82b779cbc1bffa3246f8f7a84fdc1e127c5ce8c
Fernando Tancini <fernandotancini@MBP-de-Fernando.box>
2021-06-28 09:05:07 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,32 @@ import 'package:sling_hub_flutter/view/home.dart';
 import 'package:sling_hub_flutter/view/startup_list.dart';
 import 'package:sling_hub_flutter/view/startup_detailing.dart';
 import 'package:sling_hub_flutter/view/news_list.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Future<FirebaseApp> get _fbApp => Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sling Hub',
+        title: 'Sling Hubb',
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', ''),
+          const Locale('pt', ''),
+        ],
         theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: Colors.indigo[800],
@@ -27,7 +43,16 @@ class MyApp extends StatelessWidget {
               headline6: TextStyle(fontSize: 36.0),
               bodyText2: TextStyle(fontSize: 16.0)),
         ),
-        home: HomePage(),
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
         routes: <String, WidgetBuilder>{
           StartupDetailingPage.routeName: (BuildContext context) =>
               StartupDetailingPage(),
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,6 +1,9 @@
+import 'package:camera/camera.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sling_hub_flutter/view/news_list.dart';
 import 'package:sling_hub_flutter/view/startup_list.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
   final String title = 'Sling Hub';
@@ -13,19 +16,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(
+          title: Text(
+              '${widget.title} (${AppLocalizations.of(context).helloWorld})')),
       body: ListView(children: [
         Card(
             child: ListTile(
           title: Text('Startups'),
           trailing: Icon(Icons.arrow_forward_ios_rounded),
-          onTap: () => Navigator.pushNamed(context, StartupListPage.routeName),
+          onTap: () {
+            FirebaseFirestore.instance
+                .collection('events')
+                .add({'text': 'Hola!'});
+            Navigator.pushNamed(context, StartupListPage.routeName);
+          },
         )),
         Card(
             child: ListTile(
           title: Text('Notícias'),
           trailing: Icon(Icons.arrow_forward_ios_rounded),
-          onTap: () => Navigator.pushNamed(context, NewsListPage.routeName),
+          onTap: () {
+            Navigator.pushNamed(context, NewsListPage.routeName);
+            WidgetsFlutterBinding.ensureInitialized();
+            availableCameras().then((print));
+          },
         ))
       ]),
     );
