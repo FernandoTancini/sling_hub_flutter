3eab83d4df4c290c193dce278dfc7c25e0e62dad
Fernando Tancini <fernandotancini@MBP-de-Fernando.box>
2021-06-28 19:38:59 -0300

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDo-cC8jgqyq8NTcen5r4P9aq4hu5RIGm4")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:sling_hub_flutter/view/news_list.dart';
 import 'package:sling_hub_flutter/view/startup_list.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   final String title = 'Sling Hub';
@@ -13,35 +16,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Set<Marker> markers = {
+    Marker(
+        markerId: MarkerId('123456'), position: LatLng(-22.939055, -43.1939485))
+  };
+  LatLng _initialcameraposition = LatLng(-22.9387144, -43.1931696);
+  GoogleMapController _controller;
+  Location _location = Location();
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 15),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-              '${widget.title} (${AppLocalizations.of(context).helloWorld})')),
-      body: ListView(children: [
-        Card(
-            child: ListTile(
-          title: Text('Startups'),
-          trailing: Icon(Icons.arrow_forward_ios_rounded),
-          onTap: () {
-            FirebaseFirestore.instance
-                .collection('events')
-                .add({'text': 'Hola!'});
-            Navigator.pushNamed(context, StartupListPage.routeName);
-          },
-        )),
-        Card(
-            child: ListTile(
-          title: Text('Notícias'),
-          trailing: Icon(Icons.arrow_forward_ios_rounded),
-          onTap: () {
-            Navigator.pushNamed(context, NewsListPage.routeName);
-            WidgetsFlutterBinding.ensureInitialized();
-            availableCameras().then((print));
-          },
-        ))
-      ]),
-    );
+        appBar: AppBar(
+            title: Text(
+                '${widget.title} (${AppLocalizations.of(context).helloWorld})')),
+        body: Column(
+          children: [
+            Container(
+                height: MediaQuery.of(context).size.height / 3,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(children: [
+                  Card(
+                      child: ListTile(
+                    title: Text('Startups'),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    onTap: () {
+                      FirebaseFirestore.instance
+                          .collection('events')
+                          .add({'text': 'Hola!'});
+                      Navigator.pushNamed(context, StartupListPage.routeName);
+                    },
+                  )),
+                  Card(
+                      child: ListTile(
+                    title: Text('Notícias'),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    onTap: () {
+                      Permission.location.request().isGranted.then((isGranted) {
+                        if (!isGranted) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("vsf"),
+                          ));
+                        }
+                      });
+                      Navigator.pushNamed(context, NewsListPage.routeName);
+                      WidgetsFlutterBinding.ensureInitialized();
+                      availableCameras().then((print));
+                    },
+                  )),
+                ])),
+            Container(
+                height: MediaQuery.of(context).size.height / 2,
+                width: MediaQuery.of(context).size.width,
+                child: GoogleMap(
+                  initialCameraPosition:
+                      CameraPosition(target: _initialcameraposition, zoom: 18),
+                  onMapCreated: _onMapCreated,
+                  myLocationEnabled: true,
+                  markers: markers,
+                )),
+          ],
+        ));
   }
 }
